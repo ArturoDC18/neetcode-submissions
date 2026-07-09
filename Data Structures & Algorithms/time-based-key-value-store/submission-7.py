d93fff511f27313ad25d467bc55ab8626eaa0721
Arturo Domínguez <158098659+ArturoDC18@users.noreply.github.com>
2026-07-09 14:07:05 -0600

    Add: time-based-key-value-store - submission-7

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-7.py
@@ -0,0 +1,27 @@
+class TimeMap:
+
+    def __init__(self):
+        self.hashh = {}
+        
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key in self.hashh:
+            self.hashh[key].append((timestamp,value))
+            self.hashh[key] = sorted(self.hashh[key], key=lambda item: item[0])
+        else:
+            self.hashh[key] = [(timestamp,value)]
+        
+
+    def get(self, key: str, timestamp: int) -> str:
+        if key in self.hashh:
+            l, r = 0,len(self.hashh[key])-1
+            answ = ""
+            while l <= r:
+                c = (l+r)//2
+                if self.hashh[key][c][0] <= timestamp:
+                    answ = self.hashh[key][c][1]
+                    l = c +1
+                else:
+                    r = c - 1
+            return answ
+        return ""
