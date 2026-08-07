76334fed5f92f623bf781e09f8407b3af0c80c13
Arturo Domínguez <158098659+ArturoDC18@users.noreply.github.com>
2026-08-07 13:11:43 -0600

    Add: design-word-search-data-structure - submission-4

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-4.py
@@ -0,0 +1,50 @@
+class Node:
+    def __init__(self,value):
+        self.value = value
+        self.children = dict()
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = Node('*')
+        self.activeNodes = []
+
+    def addWord(self, word: str) -> None:
+        node = self.root
+        for char in word:
+            if char in node.children:
+                node = node.children[char]
+            else:
+                new = Node(char)
+                node.children[char] = new
+                node = new
+        node.children['*'] = Node('*') 
+        
+
+    def search(self, word: str) -> bool:
+        #print("searching word: ",word)
+        self.activeNodes = [self.root]
+        for char in word:
+            nextActive = []
+            for node in self.activeNodes:
+                #print('processing node: ',node.value)
+                if char == '.':
+                    for key,child in node.children.items():
+                        nextActive.append(child)
+                else:
+                    if char in node.children:
+                        #print('found match in children')
+                        nextActive.append(node.children[char])
+            self.activeNodes = nextActive
+            if not self.activeNodes:
+                #print('No active nodes, returning False')
+                return False
+        for node in self.activeNodes:
+            if '*' in node.children:
+                #print("conclusive word FOUND, returning True")
+                return True
+        #print("Not conclusive word found")
+        return False
+        
+
+        
