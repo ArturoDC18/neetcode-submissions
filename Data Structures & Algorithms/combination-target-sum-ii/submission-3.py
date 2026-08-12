56097a7f44b8fe412722aa1b1b25f373eecdcbc4
Arturo Domínguez <158098659+ArturoDC18@users.noreply.github.com>
2026-08-12 14:13:55 -0600

    Add: combination-target-sum-ii - submission-3

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-3.py
@@ -0,0 +1,21 @@
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        answ = []
+        candidates = sorted(candidates)
+        def rec(total,path,start,last):
+            if total == target:
+                #print(f"found answer")
+                answ.append(path.copy())
+                return
+            for i in range(start,len(candidates)):
+                num = candidates[i]
+                if num=='#' or total+num > target or (i > 0 and candidates[i] == candidates[i - 1]):
+                    continue
+                path.append(num)
+                candidates[i] = '#'
+                #print(f"candidates = {candidates} num ={num} total = {total+num} last = {last}")
+                rec(total+num,path,i+1,num)
+                candidates[i] = num
+                path.pop()
+        rec(0,[],0,-1)
+        return answ
